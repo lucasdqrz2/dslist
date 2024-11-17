6d6ee863a9071ab7f558badde4e1539f6d2ec38c
lucasdqrz2 <lucasdequeirozcalado2021@hotmail.com>
2024-11-17 13:15:29 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/GameMinDTO/DTO.java
+++ src/main/java/com/devsuperior/dslist/GameMinDTO/DTO.java
@@ -25,7 +25,7 @@ public class DTO {
     public DTO(GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
-        year = projection.getYear();
+        year = projection.getGameYear();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
     }
